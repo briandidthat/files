3e7e3d38c8a4e974253d90756a57bb360383f038
briandidthat <dont@gmail.com>
2024-08-07 14:34:25 -0400

    Fix main

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
-use actix_web::{post, App, HttpResponse, HttpServer};
+use actix_web::{post, App, HttpResponse, HttpServer, Responder};
 
-#[post("/echo")]
-async fn upload_file() -> HttpResponse {
+#[post("/")]
+async fn upload_file() -> impl Responder {
     HttpResponse::Ok().body("Hello")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| { 
+    HttpServer::new(|| {
         App::new()
             .service(upload_file)
-        })
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
