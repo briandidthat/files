0ed0e973a118368afd592c2d9b81c413237ed2f3
briandidthat <blora.dev@gmail.com>
2024-08-24 20:23:43 -0400

    fix

--- src/main/java/com/briandidthat/files/service/FileService.java
+++ src/main/java/com/briandidthat/files/service/FileService.java
@@ -5,22 +5,24 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.briandidthat.files.util.FileZipper;
+
 @Service
 public class FileService {
 
-    public String uploadToFtp2(MultipartFile file) {
-        return "";
+    public String uploadFileToFtpServer(MultipartFile file) {
+        return null;
     }
 
-    public String uploadFilesToFtp2(List<MultipartFile> files, String outputFileName) {
-        return "";
+    public String uploadFilesToFtpServer(List<MultipartFile> files) {
+        return null;
     }
 
-    public String uploadToGoogleDrive(MultipartFile file) {
-        return "";
+    public String uploadFileToGoogleCloud(MultipartFile file) {
+        return null;
     }
 
-    public String uploadFilesToGoogleDrive(List<MultipartFile> file) {
-        return "";
+    public String uploadFilesToGoogleCloud(List<MultipartFile> file) {
+        return null;
     }
 }
